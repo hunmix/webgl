03e4579f8b78cf467e37b2f8749f18b4271985b9
hunmix <youarewang@126.com>
2019-08-02 22:43:13 +0800

    update coloredPoint

--- src/page/01/ColoredPoints.js
+++ src/page/01/ColoredPoints.js
@@ -16,16 +16,19 @@ function HelloPoint () {
           gl_PointSize = 10.0; // 设置尺寸
         }
       `
-      // 片元着色器
+      // 片元着色器, precision mediump float限制精度(神tm卡了我半小时)
       const FSHADER_SOURCE = `
+        precision mediump float;
+        uniform vec4 u_FragColor;
         void main() {
-          gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); // 设置颜色
+          gl_FragColor = u_FragColor; // 设置颜色
         }
       `
       utils.initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)
     }
   }, [gl])
   const paintPoint = (e) => {
+    let color
     const nativeE = e.nativeEvent
     const x = nativeE.clientX
     const y = nativeE.clientY
@@ -37,12 +40,25 @@ function HelloPoint () {
 
     const pointX = (x - left - canvasX) / canvasX // 坐标轴转换: clinet -> canvas -> webgl
     const pointY = (canvasY - (y - top)) / canvasY
-    points.push({x: pointX, y: pointY})
+    
+    if (pointX < 0 && pointY > 0) { // 第一象限
+      color = [1.0, 0.0, 0.0, 1.0]
+    } else if (pointX > 0 && pointY > 0) { // 第二象限
+      color = [0.0, 1.0, 0.0, 1.0]
+    } else if (pointX < 0 && pointY < 0) { // 第三象限
+      color = [0.0, 0.0, 1.0, 1.0]
+    } else if (pointX > 0 && pointY < 0) { // 第四象限
+      color = [1.0, 1.0, 0.0, 1.0]
+    }
+
+    points.push({x: pointX, y: pointY, color})
     const a_Position = gl.getAttribLocation(gl.program, 'a_Position')
+    const u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor')
 
     gl.clear(gl.COLOR_BUFFER_BIT) // 清空缓冲区, 缓冲区每次绘制完会丢弃
     points.forEach(point => { // 绘制所有点
       gl.vertexAttrib3f(a_Position, point.x, point.y, 0)
+      gl.uniform4f(u_FragColor, point.color[0], point.color[1], point.color[2], point.color[3])
       // 绘制一个点
       gl.drawArrays(gl.POINTS, 0, 1)
     })
--- src/router/config.js
+++ src/router/config.js
@@ -2,6 +2,7 @@ import HelloCanvas from '@page/01/HelloCanvas'
 import HelloPoint from '@page/01/HelloPoint'
 import HelloPoint2 from '@page/01/HelloPoint2'
 import ClickedPoint from '@page/01/ClickedPoint'
+import ColoredPoints from '@page/01/ColoredPoints'
 
 const routes = [
   {
@@ -19,6 +20,10 @@ const routes = [
   {
     path: '/01/ClickedPoint',
     component: ClickedPoint
+  },
+  {
+    path: '/01/ColoredPoints',
+    component: ColoredPoints
   }
 ]
 
